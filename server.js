e305b77fda7013fa973f8fe80ce8fb7daff78407
hithesh-k <hithesh.kumar@nineleaps.com>
2021-02-10 22:00:36 +0530

    [ utils ] feature added mongoose-mongoDB config

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
+require("dotenv").config();
 const express = require("express");
 
-
+const connectToDb = require("./utils/db");
 const app = express();
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
+connectToDb();
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(
   PORT,
-  console.log(`server started in ${process.env.NODE_ENV} mode at port ${PORT}`))
+  console.log(`server started in ${process.env.NODE_ENV} mode at port ${PORT}`)
+);
