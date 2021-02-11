502581086a4c1e1e31939bd566b290d60f52878b
hithesh-k <hithesh.kumar@nineleaps.com>
2021-02-11 10:18:26 +0530

    [ Controller ] feature added auth controller

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,8 @@
 
 const express = require("express");
 const router = express.Router();
-const { login, signup, me } = require("../controllers/auth");
-const { protect } = require("../middlewares/auth");
-
+const { login, signup, me } = require("../controllers/auth.controller");
+const { protect } = require("../middlewares/auth.middleware");
 router.route("/signup").post(signup);
 router.route("/login").post(login);
 router.route("/me").get(protect, me);
--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 require("dotenv").config();
 const express = require("express");
-
+const cors = require("cors");
 const connectToDb = require("./utils/db");
+const auth = require("./routes/auth");
 const app = express();
 connectToDb();
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/v1/auth", auth);
 
 const PORT = process.env.PORT || 5000;
 app.listen(
